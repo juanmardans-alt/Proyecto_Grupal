93222564a83dea5f4f09cd9fe4b6156f457ab299
Juan Martin Dans <juanmardans@gmail.com>
2026-07-03 17:44:43 -0300

    Subiendo cambios en el proyecto ServisPC

--- Proyecto Servis Pc/myapp/admin.py
+++ Proyecto Servis Pc/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Cliente, Equipo, Tecnico, Reparacion
 # Register your models here.
+admin.site.site_header = "Admin de ServisPC"
+admin.site.site_title = "Panel de Administración de ServisPC"
+admin.site.index_title = "Bienvenido al Panel de Administración de ServisPC"
+admin.site.register(Cliente)
+admin.site.register(Equipo)
+admin.site.register(Tecnico)
+admin.site.register(Reparacion)
