83824811e68c7fddce8b083464a1aa8a65e0a71e
Juan Martin Dans <juanmardans@gmail.com>
2026-07-06 21:44:44 -0300

    Subiendo cambios al repositorio remoto...

--- Proyecto Servis Pc/myapp/urls.py
+++ Proyecto Servis Pc/myapp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+
+
 from . import views
 
+
+app_name = "myapp"
 urlpatterns = [
     path('', views.index, name='index'),
-    # Aquí irán tus rutas más adelante
+    path('clientes/', views.clientes, name='clientes'),
+    path('equipos/', views.equipos, name='equipos'),
+    path('tecnicos/', views.tecnicos, name='tecnicos'),
+    path('reparaciones/', views.reparaciones, name='reparaciones')
 ]
--- Proyecto Servis Pc/myapp/views.py
+++ Proyecto Servis Pc/myapp/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .models import Cliente, Tecnico, Equipo, Reparacion
+
 
 # Create your views here.
-def index (request):
-    context = {"mensaje":"Ofrecemos servicios de reparacion de computadoras, mantenimiento y soporte tecnico."}
-    return render(request, "myapp/index.html", context)
+def index(request):
+    context = {"mensaje":"Ofrecemos servicios de reparación de computadoras, mantenimiento y soporte técnico."}
+    return render(request,"myapp/index.html",context)
+
+
+def clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'myapp/clientes.html', {'clientes': clientes})
+
+
+def equipos(request):
+    equipos = Equipo.objects.all()
+    return render(request, 'myapp/equipos.html', {'equipos': equipos})
+
+
+def tecnicos(request):
+    tecnicos = Tecnico.objects.all()
+    return render(request, 'myapp/tecnicos.html', {'tecnicos': tecnicos})
+
+
+def reparaciones(request):
+    reparaciones = Reparacion.objects.all()
+    return render(request, 'myapp/reparacion.html', {'reparaciones': reparaciones})
